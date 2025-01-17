7b80e8ea67623a7cf0ec2f9b4f660cc6253b24d4
Styles <stylesk11@gmail.com>
2025-01-16 22:30:37 -0800

    add func to fetch details for a game

--- utils/rawg.ts
+++ utils/rawg.ts
@@ -12,3 +12,16 @@ export async function fetchGames(query: string) {
   const data = await response.json();
   return data.results;
 }
+
+export async function fetchGame(id: string) {
+  const response = await fetch(
+    `${RAWG_API_BASE_URL}/games/${id}?key=${process.env.NEXT_PUBLIC_RAWG_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch game: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data;
+}
